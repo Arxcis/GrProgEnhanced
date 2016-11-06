e7dde0d6fe5ffa07c98b101f49d02c1cd5d36df4
Arxcis_MBP <jonas.solsvik@gmail.com>
2016-11-07 00:56:24 +0100

    Collapsible file containers when click on headers
    signed Jonas Solsvik

--- katalogen.php
+++ katalogen.php
@@ -8,8 +8,6 @@
 		'4_uke_oppg',
 		'5_uke_losn' 
 		);
-
-	echo 'This is PHP script';
 ?>
 
 <html>
@@ -17,30 +15,56 @@
 	    <meta charset="utf-8"/>
 	    <title> Katalogen </title>
 	    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+    	<script type="text/javascript">
+		
+			function collapseFiles(ele) {
+				var next = ele.nextSibling;
+				
+				var files = next.getElementsByClassName('file');
+
+				for (i=0; i< files.length; i++){
+
+					if (files[i].style.display == 'flex')
+						files[i].style.display = 'none';
+					else 
+						files[i].style.display = 'flex';
+
+				}
+
+			}
+		</script>
+
 	</head>
 
 	<body>
-	<div class="filcontainer">
+	<div class="fileview" >   					<!-- Begin file view -->
 		
 		<?php
 
 		foreach ($folders as $folder) {
 
-			echo '<h2>' . $folder . '</h2>';
+			echo '<div style="cursor: hand; "onclick="collapseFiles(this)"><h2 class="fileheader">' . $folder . '</h2></div>';
 			$files = scandir('katalogen/' . $folder);
 
+			echo '<div class="lefiles">';	
+												// Fill container with file-squares
 			foreach($files as $file) {
 				if($file == '.' || $file == '..' || $file == '.DS_Store'){
 					continue;
 				}
-				echo '<p>' . $file . '</p>'; 
+
+				$fullpath = 'katalogen' . DIRECTORY_SEPARATOR . $folder . DIRECTORY_SEPARATOR . $file;
+				echo '<a href="' . $fullpath . '"><div class="file">' . $file . '</div></a>'; 
 			}
-		}
 
-
+			echo '</div>';
+		}	
 		?>
 
-	</div>
+	</div>									      <!-- end file view -->
 	</body>
+
+
 </html>
