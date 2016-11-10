caf10f48094c6236042d6a7e91e0e8a0955168f5
Arxcis_MBP <jonas.solsvik@gmail.com>
2016-11-10 04:30:39 +0100

    flexbox fix

--- index.php
+++ index.php
@@ -4,15 +4,12 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>SOLDATA | Solsvik Datateknikk</title>
-		<link rel="stylesheet" type="text/css" href="css/index.css"/>
+		<link href="css/index.css" rel="stylesheet" type="text/css" />
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
 	</head>
 	<body>
 			<header>
-				S
-				<font color="#FE7E5A">O</font>
-				LDATA | 
-				<small>Solsvik Datateknikk</small>
+				S<font color="#FE7E5A">O</font>LDATA | <small>Solsvik Datateknikk</small>
 			</header>
 
 			<main class="playerinfo">
